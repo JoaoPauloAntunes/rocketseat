9de296d01b5ebb94290e4116ca44a2f847d6ba95
João Paulo Antunes <jpantunesdesouza@gmail.com>
2021-04-23 18:02:24 -0300

    feat: Adiciona título para as páginas

--- nlw/aulas/podcastrnext/src/pages/episodes/[slug].tsx
+++ nlw/aulas/podcastrnext/src/pages/episodes/[slug].tsx
@@ -2,6 +2,7 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
 import Link from 'next/link';
+import Head from 'next/head';
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { api } from '../../services/api';
@@ -31,6 +32,9 @@ export default function Episode({ episode }: EpisodeProps) {
 
   return (
     <div className={styles.episode}>
+      <Head /* Tudo que tiver aqui, é injetado no cabeçalho do HTML */> 
+        <title>{episode.title} | Podcastr</title>
+      </Head>
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
